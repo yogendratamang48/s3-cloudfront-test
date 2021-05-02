11ab4727ba6974da0b3bc3b99dfb84ba5394a36c
yogendratamang48 <yogen48@hotmail.com>
2021-05-02 22:59:09 +0200

    Add Test Build Phase in local CI

--- ci.sh
+++ ci.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/env bash
+echo "Building "
+cp -r css dist/
+cp index.html dist/
 COMMIT_HASH=`git rev-parse --short=7 HEAD`
 CF_DEV_ID='E22RDIT78RN1CE'
 echo ${COMMIT_HASH}
-aws s3 sync . s3://ytamang-may-2/${COMMIT_HASH}
+aws s3 sync ./dist s3://ytamang-may-2/${COMMIT_HASH}
 
 echo "Deploying"
 aws cloudfront get-distribution-config --id ${CF_DEV_ID} > dist.json
